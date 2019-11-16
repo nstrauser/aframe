47a5945d9c0d4cda8f024fad71d1774c76b3828d
nstrauser <40615455+nstrauser@users.noreply.github.com>
2019-11-16 12:30:19 -0800

    setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='aframe',
+      version='0.1',
+      description='Desktop frameline enerator for digital cinema cameras',
+      url='https://www.github.com/nstrauser/aframe',
+      author='Nick Strauser',
+      author_email='nstrauser@gmail.com',
+      license='MIT',
+      packages=['PySide2','bs4','lxml'],
+      zip_safe=False
+      )
